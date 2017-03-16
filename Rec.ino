d34c0f85f63856bc438e5c6a2b5e44e855aff998
baller4 <tkdbaker@yahoo.com>
2017-03-15 22:19:57 -0400

    Add files via upload

---
+++ Rec.ino
@@ -0,0 +1,100 @@
+#include <VirtualWire.h> //for RF communication
+#include <SPI.h> //for Digital Potentiometer
+
+//Iniitializes message which will store data recieved from trasmitter
+byte message[VW_MAX_MESSAGE_LEN];
+//messageLength is to assist in recieving data
+byte messageLength = VW_MAX_MESSAGE_LEN;
+
+const int ledPin = 4; //an LED is used to confirm that a signal has been recieved
+const int recPin = 7; //data line from RF reciever
+const int potPin = 10; //CS port on the digital Potentiometer
+const int speakerPin = 8;
+
+//Integer volume represents the steps (out of 256) of the digital potentiometer
+int volume = 10;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  //RF Reciever setup, from virtualwire library
+  vw_set_ptt_inverted(true);
+  vw_setup(2000);
+  vw_set_rx_pin(recPin);
+  vw_rx_start();
+  
+  pinMode(ledPin, OUTPUT);
+  
+  pinMode(potPin, OUTPUT);
+  SPI.begin(); //begin transmitting data to the digital potentiometer
+}
+
+
+void loop()
+{
+  tone(speakerPin, 1000); //begin outputing a tone from the speaker
+  
+  if (vw_get_message(message, &messageLength)) //the function "vw_get_message()" verifies a message has been recieved
+                                               // and stores the message in a variable (from virtualwire library)
+  {
+    for (int i = 0; i < messageLength; i++)//printing data recieved from RF transission
+    {
+      Serial.print(message[i]); //print the message
+      Serial.print(" ");
+      if(message[i] == 2) {
+        volume = volume + 10;
+        volume = correctVolume(volume); //see correctVolume below
+        digitalPotWrite(1, volume); //see digitalPotWrite below
+        blink();
+      }
+      if(message[i] == 3) {
+        volume = volume - 10;
+        volume = correctVolume(volume);
+        digitalPotWrite(1, volume);
+        blink();
+        blink();
+      }
+    }
+    Serial.println("Found"); //"Found" prints if a message was recieved
+  }
+  Serial.println("Loop\n"); //"Loop" prints after a cycle of void loop() is completed
+  delay(500);
+}
+
+//blink(): a blink method
+void blink()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+}
+
+/* digitalPotWrite: sets the CS pin of the digital potentiometer high to send in data
+ * the first value sent is a two bit address that specifies which channel of the DP to talk with,
+ * and the following value is the "position" of the wiper that variates the resistence between
+ * ports A and B of the specified channel
+ * Borrowed from: https://www.arduino.cc/en/Tutorial/DigitalPotControl
+ */
+void digitalPotWrite(int address, int value) {
+  digitalWrite(potPin, LOW);
+  SPI.transfer(address);
+  SPI.transfer(value);
+  digitalWrite(potPin, HIGH);
+}
+
+/*the correctVolume method makes sure the volume is not adjusted beyond the bounds
+ * of the digital potentiometer (0 - 256)
+ */
+int correctVolume(int vol) {
+  int volume = vol;
+  while(volume >= 255) {
+    volume--;
+  }
+  while(volume <= 1) {
+    volume++;
+  }
+  return volume;
+}
+
---
+++ transmitter.ino
@@ -0,0 +1,61 @@
+#include <VirtualWire.h> //for RF communication
+
+int transPin = 7;
+int buttonPinVU = 2;
+int buttonPinVD = 4;
+
+int buttonOutVU = 0; //stores the output of the increase volume button (VU: volume up)
+int buttonOutVD = 0; //stores the output of the decrease volume button (VD: volume down)
+
+boolean volumeUp = false;
+boolean volumeDown = false;
+
+void setup() 
+{
+  Serial.begin(9600);
+  
+  pinMode(buttonPinVU, INPUT);
+  pinMode(buttonPinVD, INPUT);
+
+  //RF Transmitter setup, from virtualwire library
+  vw_set_ptt_inverted(true);
+  vw_setup(2000);
+  vw_set_tx_pin(transPin);
+  analogReference(DEFAULT);//optional,not needed
+}
+
+
+void loop() 
+{
+  
+  int volumeUpCode[] = {2}; //signal that sends to increase volume
+  int volumeDownCode[] = {3}; //signal that sends to decrease volume
+  
+  buttonOutVU = digitalRead(buttonPinVU); //reads the value of the increase volume button and stores it
+  buttonOutVD = digitalRead(buttonPinVD); //reads the value of the decrease volume button and stores it
+
+  if(buttonOutVU == HIGH) { //if the volume up button is pressed, sets volumeUp true
+    volumeUp = true;
+  }
+  if(buttonOutVD == HIGH) { //if the volume down button is pressed, sets volumeDown true
+    volumeDown = true;
+  }
+
+  if(volumeUp) { //if volume up button was pressed
+    vw_send((uint8_t*)volumeUpCode, 1); //sending data through RF transimission (virtualwire function)
+    vw_wait_tx(); //waits until the message is sent (virtualwire function)
+    Serial.println("Sent"); //print "Sent" when the message has been sent
+    volumeUp = false; //reset the volumeUp boolean for the next loop
+    delay(500);
+  }
+  
+  if(volumeDown) {
+    vw_send((uint8_t*)volumeDownCode, 1); //sending data through RF transimission (virtualwire function)
+    vw_wait_tx(); //wait until the message is sent
+    Serial.println("Sent"); //prints "Sent" to confirm
+    volumeDown = false; //reset boolean value for next loop
+    delay(500);
+  }
+}
+
+
